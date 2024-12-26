6d712df53a8136cc55c1defac4fbccaab15c4995
Md Ayan Arshad <ayanarshad2002@gmail.com>
2024-12-27 00:35:41 +0530

    added code to append data

--- mycode.py
+++ mycode.py
@@ -13,6 +13,10 @@ df = pd.DataFrame(data)
 new_row_loc = {'Name':'Sufian','Age':25,'City':'RGP'}
 df.loc[len(df.index)] = new_row_loc
 
+# Adding new row to df for version 3
+new_row_loc2 = {'Name':'Saif','Age':24,'City':'RGP'}
+df.loc[len(df.index)] = new_row_loc2
+
 # Ensures that the data directory exist at the root level
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok= True)
